146d24f19c53d8490e55fed8f5eff0d7159acbee
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2021-09-01 21:32:32 -0500

    Create Agente.java
    Creación de un agente en Java, recordar que la clase debe de ir en un paquete.

---
+++ Agentes1/Agente.java
@@ -0,0 +1,15 @@
+package agente;
+
+import jade.core.Agent;
+/**
+ *
+ * @author areli
+ */
+public class Agente extends Agent
+{
+    protected void setup()
+    {
+        System.out.println("Hola Mundo del Agente");
+    }
+    
+}
