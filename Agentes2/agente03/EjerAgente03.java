e5a52139b121de4f214fb1e2ba3d8472b0009ce9
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2021-09-06 15:24:20 -0500

    Create EjerAgente03.java
    Clase en la que se crea un agente y el comportamiento en otra.

---
+++ Agentes2/agente03/EjerAgente03.java
@@ -0,0 +1,14 @@
+package agente03;
+import jade.core.Agent;
+/**
+ *
+ * @author areli
+ */
+public class EjerAgent03 extends Agent 
+{
+    protected void setup()
+    {
+        comporta03 objAg = new comporta03();
+        addBehaviour(objAg);
+    }
+}
